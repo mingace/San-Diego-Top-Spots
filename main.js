78df5b8971735727f204da8a6b3a7e40e2697ada
Nicholas Mingace <nckmingace@gmail.com>
2023-12-12 13:47:23 -0500

    First commit

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
-// write your code here
+$(document).ready(() => {
+  $.getJSON('data.json', function(data) {
+    let table = $('#tableBody');
 
+    data.forEach(function(item) {
+      let row = table[0].insertRow();
+      
+      let name = row.insertCell(0);
+      name.innerHTML = item.name;
+
+      let description = row.insertCell(1);
+      description.innerHTML = item.description;
+
+      let location = row.insertCell(2);
+      let coords = `${item.location[0]},${item.location[1]}`;
+      location.innerHTML = `<a href="https://www.google.com/maps?q=${coords}" target="_blank">Go there!</a>`;    
+    });
+  });
 });
